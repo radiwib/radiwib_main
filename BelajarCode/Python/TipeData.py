250ad78d75140a4d5483ed90d1bd2888815f2c88
radiwib <radi.wib@gmail.com>
2023-10-29 08:08:02 +0700

    commit to git

---
+++ BelajarCode/Python/TipeData.py
@@ -0,0 +1,23 @@
+# Belajar tipe data di python
+
+#integer - data angka satuan tanpa koma
+data_int = 10
+print("data = ", data_int, ", type = ", type(data_int))
+
+#float - data angka yg ada komanya
+data_float = 5.5
+print("data = ", data_float, ", type = ", type(data_float))
+
+#string - data kumpulan dari karakter
+data_string = "rahadian"
+print("data = ", data_string, ", type = ", type(data_string))
+
+#boolean - data biner (true/false)
+data_bool = True
+print("data = ", data_bool, ", type = ", type(data_bool))
+
+# Tipe2 data khusus
+
+#complex - untuk bilangan kompleks matematika
+data_complex = complex(5,9)
+print("data = ", data_complex, ", type = ", type(data_complex))
